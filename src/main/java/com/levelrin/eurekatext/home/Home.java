9e7b7c3327fa5e57b67fdfab151c2c1f0ac26ee1
Rin <levelrin@gmail.com>
2020-08-07 01:39:13 +0900

    Create category list layout. refs #3

--- src/main/java/com/levelrin/eurekatext/home/Home.java
+++ src/main/java/com/levelrin/eurekatext/home/Home.java
@@ -12,6 +12,7 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -30,7 +31,10 @@ public final class Home {
     public Scene scene() {
         final Node topBar = this.topBar();
         final Label categoriesLabel = new Label("8 categories");
-        final VBox leftBox = new VBox(new Group(categoriesLabel));
+        final VBox leftBox = new VBox(
+            new Group(categoriesLabel),
+            this.categoryList()
+        );
         leftBox.setId("left-box");
         final VBox root = new VBox(
             topBar,
@@ -59,6 +63,26 @@ public final class Home {
         root.getStylesheets().add("top-bar.css");
         root.setAlignment(Pos.CENTER_RIGHT);
         root.setId("top-bar-root");
+        return root;
+    }
+
+    /**
+     * Category list on the left.
+     * @return Category list.
+     */
+    private Node categoryList() {
+        final FontIcon square1 = new FontIcon(FontAwesome.SQUARE);
+        final Label category1 = new Label("Category 1");
+        final HBox hBox1 = new HBox(square1, category1);
+        final FontIcon square2 = new FontIcon(FontAwesome.SQUARE);
+        final Label category2 = new Label("Category 2");
+        final HBox hBox2 = new HBox(square2, category2);
+        final FontIcon square3 = new FontIcon(FontAwesome.SQUARE);
+        final Label category3 = new Label("Category 3");
+        final HBox hBox3 = new HBox(square3, category3);
+        final VBox vBox = new VBox(hBox1, hBox2, hBox3);
+        final ScrollPane root = new ScrollPane(vBox);
+        root.getStylesheets().add("category-list.css");
         return root;
     }
 
