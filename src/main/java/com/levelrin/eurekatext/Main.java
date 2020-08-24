905a9874cd4937b26e9f398284adc0e64e1d6192
Rin <levelrin@gmail.com>
2020-08-25 02:13:00 +0900

    Set title of the application. fixes #3

--- src/main/java/com/levelrin/eurekatext/Main.java
+++ src/main/java/com/levelrin/eurekatext/Main.java
@@ -19,6 +19,7 @@ public final class Main extends Application {
     @Override
     public void start(final Stage stage) {
         stage.setScene(new Home().scene());
+        stage.setTitle("EurekaText");
         stage.show();
     }
 
